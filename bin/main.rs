0707c42c37368fcafe0dafa274e97105e4963717
Pierre Krieger <pierre.krieger1708@gmail.com>
2014-11-26 10:45:59 +0100

    Move hello world to library

--- bin/main.rs
+++ bin/main.rs
@@ -1,20 +1,14 @@
 #![feature(globs)]
 #![feature(slicing_syntax)]
 
+extern crate "crates-autodoc" as autodoc;
 extern crate iron;
 
-use iron::prelude::*;
-use iron::status;
-use iron::response::modifiers::{Status, Body};
-
 use std::io::net::ip::IpAddr::Ipv4Addr;
+use iron::Iron;
 
 fn main() {
-    fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::new().set(Status(status::Ok)).set(Body("Hello World!")))
-    }
-
     let port = std::os::getenv("PORT").map(|n| from_str(n[]).unwrap()).unwrap_or(8000);
-    Iron::new(hello_world).listen((Ipv4Addr(0, 0, 0, 0), port)).unwrap();
+    Iron::new(autodoc::build_website()).listen((Ipv4Addr(0, 0, 0, 0), port)).unwrap();
     println!("Listening on {}", port);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
-#[test]
-fn it_works() {
+#![feature(globs)]
+
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+use iron::response::modifiers::{Status, Body};
+
+pub fn build_website() -> Box<iron::Handler + Send + Sync> {
+    fn hello_world(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::new().set(Status(status::Ok)).set(Body("Hello World!")))
+    }
+
+    box hello_world as Box<iron::Handler + Send + Sync>
 }
