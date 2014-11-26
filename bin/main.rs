00011f65d1b5e7eecad9726d11942505f442ae30
Pierre Krieger <pierre.krieger1708@gmail.com>
2014-11-26 10:31:48 +0100

    Fix hello world

--- bin/main.rs
+++ bin/main.rs
@@ -1,21 +1,20 @@
+#![feature(globs)]
 #![feature(slicing_syntax)]
 
-extern crate "crates-autodoc" as autodoc;
 extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+use iron::response::modifiers::{Status, Body};
 
 use std::io::net::ip::IpAddr::Ipv4Addr;
 
-use iron::Iron;
-use iron::response::modifiers::Body;
-
 fn main() {
-    fn hello_world(req: &mut iron::Request) -> iron::IronResult<iron::Response> {
-        Ok(iron::Response {
-            status: Some(iron::status::Ok),
-            .. iron::Response::new()
-        })
+    fn hello_world(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::new().set(Status(status::Ok)).set(Body("Hello World!")))
     }
 
     let port = std::os::getenv("PORT").map(|n| from_str(n[]).unwrap()).unwrap_or(8000);
-    Iron::new(hello_world).listen((Ipv4Addr(127, 0, 0, 1), port));
+    Iron::new(hello_world).listen((Ipv4Addr(0, 0, 0, 0), port)).unwrap();
+    println!("Listening on {}", port);
 }
